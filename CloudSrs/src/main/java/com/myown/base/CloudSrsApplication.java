d8f9c6ccdf426804f3fa7dc0058452b1f9950a60
sreevanikonka <saiyeshwanthkonka@gmail.com>
2019-06-02 12:16:48 +0530

    Basic project commit

---
+++ CloudSrs/src/main/java/com/myown/base/CloudSrsApplication.java
@@ -0,0 +1,13 @@
+package com.myown.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudSrsApplication.class, args);
+	}
+
+}
